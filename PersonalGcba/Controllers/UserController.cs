c4fb78bc2fede6fc9adcdcca460dfb01b2081c55
Nahuel <nahuelpastene@gmail.com>
2021-07-14 12:36:38 -0300

    Se agrego repositorio de area (para traer nombres de areas), se agrego vista Update, se corrigio en Create que la seleccion de area sea por un dropdown, se agrego en UserController metodo para listado de areas

--- PersonalGcba/Controllers/UserController.cs
+++ PersonalGcba/Controllers/UserController.cs
@@ -5,22 +5,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using PersonalGcba.Models;
 using PersonalGcba.Repository;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace PersonalGcba.Controllers
 {
     public class UserController : Controller
     {
         private readonly UserRepository _userRepository;
-        public UserController (PersonalGcbaContext context)
+        private readonly areaRepository _areaRepository;
+        public UserController(PersonalGcbaContext context)
         {
             _userRepository = new UserRepository(context);
+            _areaRepository = new areaRepository(context);
         }
         public IActionResult Index()
         {
             var empleados = _userRepository.GetAll().OrderBy(x => x.Id);
             return View(empleados);
         }
-        public IActionResult Delete (int Id)
+        public IActionResult Delete(int Id)
         {
             Empleado empleado = _userRepository.GetById(Id);
             _userRepository.Delete(empleado);
@@ -30,12 +33,19 @@ namespace PersonalGcba.Controllers
         [HttpGet]
         public IActionResult Create()
         {
+            ViewBag.listaAreas = new SelectList(GetAreas(), "IdArea", "NombreArea");
             return View("Create");
+        }
+        public IList<Area> GetAreas()
+        {
+            List<Area> listaAreas = _areaRepository.GetAll().ToList();
+            return listaAreas;
         }
         [HttpPost]
         public IActionResult Create(Empleado empleado)
         {
-            if (ModelState.IsValid){
+            if (ModelState.IsValid)
+            {
                 _userRepository.Create(empleado);
                 _userRepository.SaveChanges();
                 return RedirectToAction("Index", "Home");
@@ -46,28 +56,27 @@ namespace PersonalGcba.Controllers
         public IActionResult Update(int Id)
         {
             var empleado = _userRepository.GetById(Id);
-            return View("Update", empleado);
+            ViewBag.list = _areaRepository.GetAll();
+            return View(empleado);
         }
         [HttpPost]
         public IActionResult Update(Empleado empleado, int Id)
         {
+            var user = _userRepository.GetById(empleado.Id);
             if (ModelState.IsValid)
             {
-                var user = _userRepository.GetById(empleado.Id);
-                if (user != null)
-                {
-                    user.Nombre = empleado.Nombre;
-                    user.Apellido = empleado.Apellido;
-                    user.Dni = empleado.Dni;
-                    user.FechaNacimiento = empleado.FechaNacimiento;
-                    user.Desarrollador = empleado.Desarrollador;
-                    user.Descripcion = empleado.Descripcion;
-                    user.IdArea = empleado.IdArea;
-                    _userRepository.Update(empleado);
-                    _userRepository.SaveChanges();
-                }
+                user.Nombre = empleado.Nombre;
+                user.Apellido = empleado.Apellido;
+                user.Dni = empleado.Dni;
+                user.FechaNacimiento = empleado.FechaNacimiento;
+                user.Desarrollador = empleado.Desarrollador;
+                user.Descripcion = empleado.Descripcion;
+                user.IdArea = empleado.IdArea;
+                _userRepository.Update(user);
+                _userRepository.SaveChanges();
                 return RedirectToAction("Index", "Home");
             }
+            ViewBag.list = _areaRepository.GetAll();
             return View(empleado);
         }
     }
---
+++ PersonalGcba/Repository/areaRepository.cs
@@ -0,0 +1,30 @@
+﻿using PersonalGcba.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalGcba.Repository
+{
+    public class areaRepository
+    {
+        private readonly PersonalGcbaContext _context;
+        public areaRepository(PersonalGcbaContext context)
+        {
+            _context = context;
+        }
+        public Area GetById(int Id)
+        {
+            Area area = _context.Areas.Where(x => x.IdArea == Id).FirstOrDefault();
+            return area;
+        }
+        public IEnumerable<Area> GetAll()
+        {
+            return _context.Areas;
+        }
+        internal void GetById(Area area)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
